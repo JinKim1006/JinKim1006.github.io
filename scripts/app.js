366caf76a94f21dc3ada368c602e1e7dce6f145c
Jin <kimsj7367@gmail.com>
2023-01-18 19:35:10 -0500

    ICE 01 - First Commit

--- scripts/app.js
+++ scripts/app.js
@@ -37,35 +37,56 @@
             //console.log("About Us Button Clicked");
             location.href = "about.html"
         });
-    }
-    function DisplayProductPage() {
         let ProductButton = document.getElementById("ProductBtn");
         ProductButton.addEventListener("click", function () {
             //console.log("About Us Button Clicked");
             location.href = "product.html"
         });
-    }
-    function DisplayServicePage() {
         let ServiceButton = document.getElementById("ServiceBtn");
         ServiceButton.addEventListener("click", function () {
             //console.log("About Us Button Clicked");
             location.href = "service.html"
         });
-    }
-    function DisplayContactPage() {
         let ContactUsButton = document.getElementById("ContactUsBtn");
         ContactUsButton.addEventListener("click", function()
         {
             //console.log("About Us Button Clicked");
             location.href = "contact.html"
         });
+
+        // step 3
+        let MainContent = document.getElementsByTagName("main")[0];
+        // step 4
+        let MainParagraph = document.createElement("p");
+        // step 5
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+        MainParagraph.textContent = "This is the Main Paragraph!";
+        // step 6
+        MainContent.appendChild(MainParagraph);
+        // step 7
+        let FirstString = "This is";
+        let SecondString = `${FirstString} the Main Paragraph.`;
+        MainParagraph.textContent = SecondString;
+        // step 8
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is my article paragraph</p>`;
+        Article.setAttribute("class", "container");
+        Article.innerHTML= ArticleParagraph;
+        document.body.appendChild(Article);
+    }
+
+    function DisplayProductPage() {
+
+    }
+    function DisplayServicePage() {
+
+    }
+    function DisplayContactPage() {
+
     }
     function DisplayAboutPage() {
-        let AboutUsButton = document.getElementById("AboutUsBtn");
-        AboutUsButton.addEventListener("click", function () {
-            //console.log("About Us Button Clicked");
-            location.href = "about.html"
-        });
+
     }
 
 })();
